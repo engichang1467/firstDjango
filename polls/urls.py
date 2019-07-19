bdf8e2e06c1b6dd56a7ef51e1e2686174cfd9093
Michael Chang <mchang0926@gmail.com>
2019-07-19 14:12:22 -0700

    finish part 2

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
